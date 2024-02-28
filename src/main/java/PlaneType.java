f9f8388e3af1259e6f239f321a0693e3aa72e6a2
Chamsron <emmacameron219@gmail.com>
2024-02-28 14:44:55 +0000

    Creating Enum class for plane types

---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,19 @@
+public enum PlaneType {
+    BOEING747(400, 800);
+
+    private final int capacity;
+    private final int totalWeight;
+
+    PlaneType(int capacity, int totalWeight){
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getTotalWeight() {
+        return totalWeight;
+    }
+}
